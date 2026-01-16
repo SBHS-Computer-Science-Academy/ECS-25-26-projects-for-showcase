56ae495f18b318c82a051f1fc5d966e7215bba70
Sky Adams <skyadams@sbunified.org>
2026-01-16 09:55:59 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Class-Year-projects-for-showcase-template
+https://sbhs-computer-science-academy.github.io/Class-YY-YY-projects-for-showcase/
---
+++ script.js
@@ -0,0 +1,64 @@
+// Each name should be in the format "Stu1, Stu2, and Stu3" with an optional URL
+var names = [
+    ["CSA_logo", "https://www.sbhscs.org"]
+];
+
+var gridWidth = 4;
+var gridHtml = "";
+var idx = 0;
+
+makeGrid();
+
+function generateCard(names, imageName, altText, url)
+{
+    return '<div class="card text-center"> <div class="card-body"> <h5 class="card-title">'
+    + names
+    + '</h5> <p class="card-text"> <a href="'
+    + url
+    + '" target="_blank" rel="noopener noreferrer"> <img src="images/'
+    + imageName
+    + '" alt="'
+    + altText
+    + '" class="screenshots"> </a> </p> </div> </div>';
+}
+
+function makeCard() {
+    if (idx >= names.length) return;
+    var name = names[idx];
+    var fileUrl = "";
+    if (name.constructor === Array) {
+    	name = names[idx][0];
+    	fileUrl = names[idx][1];
+    }
+    var file = name.replaceAll(" ","").replaceAll(",","").replace("and","");
+    var imageName = file + ".png"
+    
+    if (fileUrl != "") file = fileUrl;
+    var card = generateCard(name, imageName, name, file);
+    gridHtml += card; //not elegant, but it works
+    
+    idx++;
+}
+
+
+function makeGrid() {
+    
+    for (var i = 0; i < names.length; i++) {
+        if (i % gridWidth == 0) {
+            gridHtml += '<div class="row">';
+        }
+        gridHtml += '<div class="col-md-3 p-3">';
+        makeCard();
+        gridHtml += '</div>';
+        if (i % gridWidth == (gridWidth - 1)) {
+            gridHtml += '</div>';
+        }
+    }
+    
+    if (names.length % gridWidth != 0) {
+        gridHtml += '</div>';
+    }
+    
+    document.getElementById("grid").innerHTML += gridHtml;
+    
+}
